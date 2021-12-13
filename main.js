274a6846748ce0162ed31e7a9c67a3d07f9fbeb9
Luís Fernando Grange Ferreira <luisfernando237grange@outlook.com>
2021-12-13 11:19:38 -0300

    Update files

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const link = $('#url');
+const newURL = $('.link .aux a');
 
 $('.shorten').on('click', async function(){
-    if(link.val() != undefined){
+    if(!link.val()){
+        link.addClass('error');
+        $('.error-msg').css('display', 'block'); 
+    }
+
+    else{
         let result = await fetch(`https://api.shrtco.de/v2/shorten?url=${link.val()}`, {method: 'POST'})
         let json = await result.json();
 
@@ -14,11 +20,12 @@ $('.shorten').on('click', async function(){
             <a target="_blank" href="${link.val()}">${link.val()}</a>
             <div class="aux">
                 <a target="_blank" href="${newJson['result']['full_short_link']}">${newJson['result']['full_short_link']}</a>
-                <button>Copy</button>
+                <button class="copy-btn">Copy</button>
             </div>
             </li>
         </div>
         `);
-
+        $('.error-msg').css('display', 'none');
+        link.removeClass('error');
     }
 });
